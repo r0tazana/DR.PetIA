8bc4d390d3fc14eb9bc72e574b85f2af90a71ea0
Rodrigo <rodrigo.nif.oliveira@gmail.com>
2026-09-05 18:28:25 -0300

    feat: criação de estrutura do formulario de entrada no streamlit

--- pet.py
+++ pet.py
@@ -8,3 +8,21 @@ load_dotenv()
 
 #Estabelece a conecção com a API da OpenAI usando a chave salva posteriormente
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+#Apresentação e estrutura da pag do app
+
+st.title("🐾 Dr.PetIA - Triagem Veterinária")
+st.write("Preencha as informações do seu pet para receber uma orientação preliminar.")
+
+st.divider()
+
+#Organização da entrada de dados
+col1, col2 =st.columns(2)
+
+with col1:
+    espécie = st.selectbox("Espécie:", ["Cachorro", "Gato","Coelho","Hamster","Peixe","Tartaruga","Calopsita","Papagaio","Outros"])
+    idade = st.text_input("Idade Aproximada:", placeholder="Ex: 2 anos")
+
+with col2:
+    porte = st.selectbox("Porte de Animal:", ["Pequeno","Médio","Grande"])
+    sintomas = st.text_area("Sintomas Observados:", placeholder="apatia e falta de apetite")
